03abaa16675507d4db629fbf5a4cf22f10d96e95
Haner72 <gustavo_alwunder@hotmail.com>
2024-10-31 16:06:49 -0300

    data bank migration (still don't work affe)

--- backend/src/main/java/com/data/collection/model/UserModel.java
+++ backend/src/main/java/com/data/collection/model/UserModel.java
@@ -1,11 +1,13 @@
 package com.data.collection.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
