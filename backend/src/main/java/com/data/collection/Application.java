b53590507860b98a0947b5f22279df08fd5f4cda
Haner72 <gustavo_alwunder@hotmail.com>
2024-11-10 15:17:20 -0300

    screen and application adjustment

--- backend/src/main/java/com/data/collection/Application.java
+++ backend/src/main/java/com/data/collection/Application.java
@@ -1,5 +1,8 @@
 package com.data.collection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,6 +11,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.data.collection.repository")
 public class Application {
     public static void main(String[] args) {
+        try {
+            Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "admin");
+            if (conexao != null) {
+                System.out.println("Deu certo uhuuul!");
+            } else {
+                System.out.println("Deu errado affe");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
         SpringApplication.run(Application.class, args);
     }
 }
